21c1e77804a84d0309155437ee9cdf577edeb9ca
Xurshid-Malikov <maliovxurshid@gmail.com>
2022-06-16 21:31:27 +0500

    create dang

---
+++ js/main.js
@@ -0,0 +1,140 @@
+// ------------------------------------------------------1-Masala---------------------------------------------------
+// Sharti:
+// addition(3, 2) ➞ 5
+// addition(-3, -6) ➞ -9
+// addition(7, 3) ➞ 10
+
+// Javobi:
+
+// function addition(a, b) {
+//     return a + b
+// }
+// console.log(addition(3,2))
+
+
+//-------------------------------------------------------2-Masala---------------------------------------------------
+// Sharti:
+// convert(5) ➞ 300
+// convert(3) ➞ 180
+// convert(2) ➞ 120
+
+// Javobi:
+// function convert(minutes) {
+// 	var sec = 60
+// 	return minutes * sec
+// }
+// console.log(convert(5))
+
+
+//-------------------------------------------------------3-Masala------------------------------------------------
+// Sharti:
+// addition(0) ➞ 1
+// addition(9) ➞ 10
+// addition(-3) ➞ -2
+
+// Javobi:
+// function addition(num) {
+// 	return ++num
+// }
+// console.log(addition(9))
+
+
+//----------------------------------------------------4-Masala----------------------------------------------------
+// Sharti:
+// triArea(3, 2) ➞ 3
+// triArea(7, 4) ➞ 14
+// triArea(10, 10) ➞ 50
+
+// Javobi:
+// function triArea(base, height) {
+// 	return (base * height) / 2
+// }
+// console.log(triArea(3,2))
+
+
+//----------------------------------------------------5-Masala-----------------------------------------------------
+// Sharti:
+// calcAge(65) ➞ 23725
+// calcAge(0) ➞ 0
+// calcAge(20) ➞ 7300
+
+// Javobi:
+// function calcAge(age) {
+// 	var kun = 365
+// 	return age * kun
+// }
+// console.log(calcAge(65))
+
+
+//---------------------------------------------------6-Masala-----------------------------------------------------
+// Sharti:
+// remainder(1, 3) ➞ 1
+// remainder(3, 4) ➞ 3
+// remainder(-9, 45) ➞ -9
+// remainder(5, 5) ➞ 0
+
+// Javobi:
+// function remainder(x, y) {
+// 	return x % y
+// }
+// console.log(remainder(1,3))
+
+
+//-------------------------------------------------7-Masala--------------------------------------------------------
+// Sharti:
+// footballPoints(3, 4, 2) ➞ 13
+// footballPoints(5, 0, 2) ➞ 15
+// footballPoints(0, 0, 1) ➞ 0
+
+// Javobi:
+// function footballPoints(wins, draws, losses) {
+// 	var w = wins * 3
+// 	var d = draws * 1
+// 	var l = losses * 0
+// 	return w + d + l
+// }
+// console.log(footballPoints(3,4,2))
+
+
+//------------------------------------------------8-Masala-------------------------------------------------------
+// Sharti:
+// points(1, 1) ➞ 5
+// points(7, 5) ➞ 29
+// points(38, 8) ➞ 100
+
+// Javobi:
+// function points(twoPointers, threePointers) {
+// 	var two = twoPointers * 2
+// 	var three = threePointers * 3
+// 	return two + three
+// }
+// console.log(points(1,1))
+
+
+//-----------------------------------------------9-Masala---------------------------------------------------------
+// Sharti:
+// lessThan100(22, 15) ➞ true
+// 22 + 15 = 37
+// lessThan100(83, 34) ➞ false
+// 83 + 34 = 117
+// lessThan100(3, 77) ➞ true
+
+// Javobi:
+// function lessThan100(a, b) {
+// 	return (a + b) < 100
+// }
+// console.log(lessThan100(22,15))
+
+//--------------------------------------------------10-Masala-----------------------------------------------------
+// Sharti:
+// animals(2, 3, 5) ➞ 36
+// animals(1, 2, 3) ➞ 22
+// animals(5, 2, 8) ➞ 50
+
+// Javobi:
+// function animals(chickens, cows, pigs) {
+// 	var ch = chickens * 2
+// 	var c = (cows + pigs) * 4
+// 	return ch + c
+// }
+// console.log(animals(2,3,5))
